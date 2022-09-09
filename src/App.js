3cc8185827f129f5850b18aac9d3e726a1502d8c
rixy-ynxy <ryuji_tokiwa@yunixy.co.jp>
2022-09-10 02:17:17 +0900

    reactstrap add edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import {Container, Card, CardBody, CardTitle, Button, CardText, Row, Col} from 'reactstrap';
+
+const products = [{
+  title: '商品1',
+  description: '商品説明文',
+  price: 100,
+}, {
+  title: '商品2',
+  description: '商品説明文',
+  price: 150,
+}, {
+  title: '商品3',
+  description: '商品説明文',
+  price: 200,
+}]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React Stripe Amplify app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+          <Row md="3">
+            {products.map((product, i) => (
+                <Col key={i}>
+                  <Card>
+                    <CardBody>
+                      <CardTitle>{product.title}</CardTitle>
+                      <CardText>{product.description}</CardText>
+                      <Button>注文する ({product.price} 円)</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+          </Row>
+        </Container>
     </div>
   );
 }
