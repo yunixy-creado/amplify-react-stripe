d8db71aea39a65dd6c7f8a1108e982e7d8506d76
rixy-ynxy <ryuji_tokiwa@yunixy.co.jp>
2022-09-10 15:53:01 +0900

    add api product list stripe link

--- amplify/backend/function/amplifyStripeFunction/src/app.js
+++ amplify/backend/function/amplifyStripeFunction/src/app.js
@@ -1,4 +1,18 @@
 /*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["STRIPE_LIMITED_API_KEY"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
+/*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
@@ -9,6 +23,28 @@ See the License for the specific language governing permissions and limitations 
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+const aws = require('aws-sdk')
+async function loadStripeAPIKey() {
+  const { Parameter } = await (new aws.SSM())
+    .getParameter({
+      Name: process.env["STRIPE_LIMITED_API_KEY"],
+      WithDecryption: true,
+    })
+    .promise()
+
+    const stripeApiKey = Parameter.Value
+    if (!stripeApiKey) {
+      throw new Error('STRIPE_LIMITED_API_KEYがSecret valuesに設定されていません。')
+    }
+
+    return stripeApiKey
+}
+
+async function loadStripe() {
+  const apiKey = await loadStripeAPIKey()
+  return require('stripe')(apiKey)
+}
 
 // declare a new express app
 var app = express()
@@ -26,9 +62,18 @@ app.use(function(req, res, next) {
 /**********************
  * 商品一覧を取得するAPI *
  **********************/
-app.get('/shop/products', function(req, res) {
+app.get('/shop/products', async function(req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  const stripe = await loadStripe()
+  const items = await stripe.products.list()
+  const response = await Promise.all(items.data.map(async product => {
+    const priceResponse = await stripe.prices.list({
+      product: product.id
+    })
+    product.prices = priceResponse.data
+    return product
+  }))
+  res.json(response);
 });
 
 /****************************
